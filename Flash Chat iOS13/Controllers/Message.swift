18ec20bd4202c94ff99751e1e5200899a3c61370
Carlos-Georgescu <dr.cag10@gmail.com>
2020-06-15 03:27:54 -0700

    first commit

---
+++ Flash Chat iOS13/Controllers/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Carlos Georgescu on 2020-06-15.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,9 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Carlos Georgescu on 2020-06-15.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
